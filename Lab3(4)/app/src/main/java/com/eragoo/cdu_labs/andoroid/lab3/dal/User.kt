4adadb7b4facf4d5d2b8eeecd4eaaf6334ae6f64
Oleksandra Dovhal <62622145+OleksandraDovhal@users.noreply.github.com>
2022-11-05 22:25:59 +0200

    Add files via upload

---
+++ Lab3(4)/app/src/main/java/com/eragoo/cdu_labs/andoroid/lab3/dal/User.kt
@@ -0,0 +1,13 @@
+package com.eragoo.cdu_labs.andoroid.lab3.dal
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User(
+    @PrimaryKey val uid: String,
+    @ColumnInfo(name = "first_name") val firstName: String,
+    @ColumnInfo(name = "last_name") val lastName: String,
+    @ColumnInfo(name = "email") val email: String
+)
---
+++ Lab3(4)/app/src/main/java/com/eragoo/cdu_labs/andoroid/lab3/dal/UserDto.kt
@@ -0,0 +1,3 @@
+package com.eragoo.cdu_labs.andoroid.lab3.dal
+
+data class UserDto(val name: String, val surname: String, val email: String){}
