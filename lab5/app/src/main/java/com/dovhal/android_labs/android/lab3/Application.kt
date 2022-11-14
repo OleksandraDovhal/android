5831ba31b507157720788db799be4692f98b8d96
Oleksandra Dovhal <sasha.dovgal.2002@gmail.com>
2022-11-14 21:22:12 +0200

    Fix city repeat bug

--- lab5/app/src/main/java/com/dovhal/android_labs/android/lab3/Application.kt
+++ lab5/app/src/main/java/com/dovhal/android_labs/android/lab3/Application.kt
@@ -2,11 +2,16 @@ package com.dovhal.android_labs.android.lab3
 
 import android.app.Application
 import com.dovhal.android_labs.android.lab3.di.SingletonHolder
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         SingletonHolder.init(this)
+        GlobalScope.launch {
+            SingletonHolder.cityRepository.deleteAll()
+        }
     }
 }
